1da17d2fa07c687dcd3cb3fafb8d94a5a1f37725
Matt Ackerman <matt@808.org>
2022-03-15 17:43:15 +0000

    remove dupe line and unused variable

--- cgi-bin/setup_db.py
+++ cgi-bin/setup_db.py
@@ -6,8 +6,6 @@ import datetime
 
 sqlite_file = '/var/tmp/chores.sqlite'
 
-daytime = datetime.datetime.now()
-
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 c.execute("PRAGMA auto_vacuum = 2;")
@@ -16,7 +14,6 @@ c.execute("PRAGMA temp_store = MEMORY;")
 c.execute("PRAGMA journal_mode = MEMORY;")
 c.execute("CREATE TABLE IF NOT EXISTS completed_chores (id INTEGER PRIMARY KEY, name TEXT, chore TEXT, date TEXT, value TEXT);")
 c.execute("CREATE TABLE IF NOT EXISTS chore_table (chore TEXT, value TEXT);")
-c.execute("CREATE TABLE IF NOT EXISTS users (name TEXT PRIMARY KEY, gauth TEXT, email TEXT);")
 c.execute("CREATE TABLE IF NOT EXISTS users (name TEXT PRIMARY KEY, gauth TEXT, email TEXT);")
 c.execute("VACUUM;")
 conn.commit()
